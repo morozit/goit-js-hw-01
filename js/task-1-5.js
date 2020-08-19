76e2717de2e5565b51a62af4bdbbc362e1b6e8cf
Roman <65686226+morozit@users.noreply.github.com>
2020-08-20 00:59:03 +0300

    Update task-1-5.js

---
+++ js/task-1-5.js
@@ -0,0 +1,37 @@
+const countryName = prompt("Выберите страну (Китай, Австралия, Индия, Ямайка");
+
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const NO_DELIVERY = 'В выбранную страну доставка недоступна.';
+const CHINA = 'Китай';
+const AUSTRALIA = 'Австралия';
+const INDIA = 'Индия';
+const JAMAICA = 'Ямайка';
+let message;
+let price = 0;
+let country;
+if (countryName === null) {
+  message = CANCELED_BY_USER;
+} else {
+  country = countryName[0] + countryName.slice(); // Write code on this line
+  switch (country) {
+  // Write code under this line
+    
+  }
+}
+if () { // Write code on this line
+  message = `Доставка в ${country} будет стоить ${price} кредитов`;
+}
+
+console.log(message);
+
+//если countryName равно "КитаЙ"
+// то значение message будет равно
+// 'Доставка в Китай будет стоить 100 кредитов'
+
+//если countryName равно null
+// то значение message будет равно
+// 'Отменено пользователем!'
+
+//если countryName равно "Чили"
+// то значение message будет равно
+// 'В выбранную страну доставка недоступна.'
