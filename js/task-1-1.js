8bde25dbeb8968c0725b21d71664fe8a8200717b
Roman <65686226+morozit@users.noreply.github.com>
2020-08-20 22:19:06 +0300

    Delete task-1-1.js

--- js/task-1-1.js
+++
@@ -1,9 +0,0 @@
-const name = "«Генератор защитного поля»";
-let price = 1000;
-const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`;
-price = 2000;
-const secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`;
-
-console.log(firstMessage);
-
-console.log(secondMessage);
