e1a34d72f9d1610ff4ab922136336edd66b8438f
Roman <65686226+morozit@users.noreply.github.com>
2020-08-20 22:19:08 +0300

    Delete task-1-2.js

--- js/task-1-2.js
+++
@@ -1,9 +0,0 @@
-const invoice = 100;
-const stock = 100;
-
-const message =
-  stock >= invoice
-    ? "{Заказ оформлен, с вами свяжется менеджер}"
-    : "{На складе недостаточно товаров!}";
-
-console.log(message);
