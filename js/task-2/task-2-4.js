d8d077bbf8a7a4b5b81d8fb2e2193affd9d8ca7d
Roman <65686226+morozit@users.noreply.github.com>
2020-08-22 21:10:18 +0300

    Update task-2-4.js

---
+++ js/task-2/task-2-4.js
@@ -0,0 +1,20 @@
+function formatString(string, maxLength = 40) {
+  // Write code under this line
+  if (string.length < maxLength) {
+    return string;
+  }
+  if (string.length > maxLength) {
+    return string.slice(0, -(string.length - maxLength)) + "...";
+  }
+}
+
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// 'Curabitur ligula sapien, tincidunt non.'
+
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+// 'Vestibulum facilisis, purus nec pulvinar...'
+
+console.log(
+  formatString("Vestibulum facilisis, purus nec pulvinar iaculis.", 30)
+);
+// 'Vestibulum facilisis, purus ne...'
