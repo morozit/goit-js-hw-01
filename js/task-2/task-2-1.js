cda80902bf0c8a2d674dd2ca8441f39daff7d4df
Roman <65686226+morozit@users.noreply.github.com>
2020-08-22 18:45:53 +0300

    Update task-2-1.js

--- js/task-2/task-2-1.js
+++ js/task-2/task-2-1.js
@@ -20,7 +20,7 @@ console.log(getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
 '
 */
 
-// console.log(getItemsString([5, 10, 15]));
+console.log(getItemsString([5, 10, 15]));
 /*
 '1 - 5
 2 - 10
