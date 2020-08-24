6eecbf7ba9b37d7516c55cfdd1d3e9fc95a0e8d7
Roman <65686226+morozit@users.noreply.github.com>
2020-08-24 23:31:05 +0300

    Update task-2-6.js

--- js/task-2/task-2-6.js
+++ js/task-2/task-2-6.js
@@ -1,7 +1,10 @@
 function mapArray(array) {
   "use strict";
   const numbers = new Array(array.length);
-  for (let i = 0; i < array.length; i += 1) {}
+  for (let i = 0; i < array.length; i += 1) {
+    numbers[i] = array[i] * 10;
+  }
+
   return numbers;
 }
 
