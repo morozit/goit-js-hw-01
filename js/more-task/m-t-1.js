376dd690231af5282cd2a4520ee39ed1cd3973a1
Roman <65686226+morozit@users.noreply.github.com>
2020-08-24 08:18:20 +0300

    Update m-t-1.js

--- js/more-task/m-t-1.js
+++ js/more-task/m-t-1.js
@@ -4,18 +4,32 @@
 2 - Розбивання по словам
 3 - Зшиваєм з  дефісом 
 */
-const title = "Top 10 benefits of React framework";
+// const title = "Top 10 benefits of React framework";
 
 // TODO: 1 variant
-const normalizedTitle = title.toLocaleLowerCase();
+// const normalizedTitle = title.toLocaleLowerCase();
+// const words = normalizedTitle.split(" ");
+// const slug = words.join("-");
 
-const words = normalizedTitle.split(" ");
-
-const slug = words.join("-");
-
-console.log(slug);
+// console.log(slug);
 
 //  TODO: 2 variant
-const slug1 = title.toLowerCase().split(" ").join("-");
+const title1 = "Top 10 benefits of React framework";
+
+const slug1 = title1.toLowerCase().split(" ").join("-");
 
 console.log(slug1);
+
+// TODO: 3 variant
+
+const slugify = function (string) {
+  // const normalizedTitle = string.toLocaleLowerCase();
+  // const words = normalizedTitle.split(" ");
+  // const slug = words.join("-");
+
+  return string.toLowerCase().split(" ").join("-");
+};
+
+console.log(slugify("На всю дистанцію команда бере один комплект мотузок"));
+console.log(slugify("який міняти або залишати між блоками заборонено"));
+console.log(slugify("Всі блоки обладнанні лініями старту і фінішу"));
