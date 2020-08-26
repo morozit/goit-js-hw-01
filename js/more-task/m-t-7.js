c7d3392e833cdc01c695ec1710bb8ab2e33b99a9
Roman <65686226+morozit@users.noreply.github.com>
2020-08-27 00:18:23 +0300

    Update m-t-7.js

--- js/more-task/m-t-7.js
+++ js/more-task/m-t-7.js
@@ -3,3 +3,8 @@ const fn = function () {};
 fn(1, 2, 3, 4, 5, 6, 7);
 fn(1, 2, 3);
 fn(1, 2, 3, 4, 5);
+
+// ? парс числа     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+let element = "50.25";
+const result = Number.parseInt(element);
+console.log(result);
