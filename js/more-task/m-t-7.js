a6cfcac549431c28acdb5f3ddba1446728847747
Roman <65686226+morozit@users.noreply.github.com>
2020-08-27 16:50:40 +0300

    Update m-t-7.js

--- js/more-task/m-t-7.js
+++ js/more-task/m-t-7.js
@@ -1,10 +1,33 @@
-const fn = function () {};
-
-fn(1, 2, 3, 4, 5, 6, 7);
-fn(1, 2, 3);
-fn(1, 2, 3, 4, 5);
-
 // ? парс числа     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-let element = "50.25";
-const result = Number.parseInt(element);
-console.log(result);
+// let element = "50.251212122111123";
+// const result = Number.parseInt(element);
+// console.log(result);
+
+// ? відсікає кількість значень після крапки
+// let selary = "5012.1234";
+// // selary = selary.toFixed(2);
+// selary = Number(selary.toFixed(2));
+// console.log(selary);
+
+// ? обєкт Math   pow виводить в степені, є аналог сучасніший метод  ==>  **  <==
+// //TODO: 1 variant
+// const a = 3;
+// const b = 5;
+// const result = Math.pow(a, b);
+// console.log(result);
+// //TODO: 2 variant
+// const a = 3;
+// const b = 5;
+// let result = 3 ** 5;
+// console.log(result);
+
+// ? Скріпт який просить користувача ввести число та степінь. Потім виконається бчислення і виводить результат в консоль
+
+// let base = prompt("Введіть число");
+// base = Number(base);
+
+// let power = prompt("Введіть степінь");
+// power = Number(power);
+
+// const result = base ** power;
+// console.log(result);
